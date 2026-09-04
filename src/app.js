ae669453fce4e75e3814f30bc5b4a91d71676a91
Emerson <emersonrodriguez1455@gmail.com>
2026-09-04 15:46:31 -0600

    camibos

--- src/app.js
+++ src/app.js
@@ -7,19 +7,19 @@ import ticketRoutes from './routes/ticketRoutes';
 dontenv.config();  //configura laas variables de entorno
 const app = express(); //crea las instancias para levantar el servidor
 
-app.use(express.json()); //transforma las llamadas a json
-app.use(cors()) //permite conexiones futuras con el frontend seguras sin las restricciones
+app.use(express.json()); // transforma las llamadas a json
+app.use(cors()); //permite conexiones futuras con el frontend seguras sin las restricciones
 
 
 //configuracion de como aparecera el url
 
 //app.use(nombre que aparecera en la web 'api/rutas', el nombre de la variable)
 
-app.use('api/auth',authRoutes)
-app.use('api/tickets',ticketRoutes)
+app.use('api/auth',authRoutes);
+app.use('api/tickets',ticketRoutes);
 
-const PORT = process.env.PORT || 3000 //decimos que que use el puerto de nuestro .env o que se conecte en el puerto 3000 localhost
+const PORT = process.env.PORT || 3000; // decimos que que use el puerto de nuestro .env o que se conecte en el puerto 3000 localhost
 
 app.listen(PORT, ()=>{
-    console.log(`sevidor corriendo en ${PORT}`)
+    console.log(`sevidor corriendo en ${PORT}`);
 })
